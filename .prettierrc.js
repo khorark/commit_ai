a3feb777d26c48512b3393494c99ceb53158be5a
Khoroshikh Arkadii <a.khoroshikh@cs.money>
2024-04-26 20:32:30 +0300

    Configure project

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  printWidth: 120,
+  jsxSingleQuote: true,
+  endOfLine: 'auto',
+};
